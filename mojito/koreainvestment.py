fce97c3e565caf45c38645d1e145f321e7d5d440
Brayden Jo <brayden.jo@outlook.com>
2022-04-02 11:55:04 +0900

    한국투자증권 Open API 기본 코드

---
+++ mojito/koreainvestment.py
@@ -0,0 +1,67 @@
+from mojito.base.broker import Broker 
+import requests 
+import json
+
+
+class KoreaInvestment(Broker):
+    BASE_URL = "https://openapi.koreainvestment.com:9443"
+    BASE_URL_TEST = "https://openapivts.koreainvestment.com:29443"
+
+    def __init__(self, api_key: str, api_secret: str):
+        self.api_key = api_key 
+        self.api_secret = api_secret
+        self.issue_access_token() 
+
+    def issue_access_token(self):
+        """접근토큰발급
+        """
+        path = "oauth2/tokenP"
+        headers = {"content-type":"application/json"}
+        body = {
+            "grant_type":"client_credentials",
+            "appkey": self.api_key, 
+            "appsecret": self.api_secret
+        }
+        url = f"{self.BASE_URL}/{path}"
+        resp = requests.post(url, headers=headers, data=json.dumps(body))
+        self.access_token = 'Bearer {}'.format(resp.json()["access_token"])
+
+    def fetch_price(self, market_code: str, ticker: str) -> dict:
+        """주식현재가시세
+
+        Args:
+            market_code (str): 시장 분류코드
+            ticker (str): 종목코드
+
+        Returns:
+            dict: API 개발 가이드 참조 
+        """
+        path = "uapi/domestic-stock/v1/quotations/inquire-price"
+        headers = {
+           "content-type":"application/json", 
+           "authorization": self.access_token,
+           "appKey": self.api_key,
+           "appSecret": self.api_secret,
+           "tr_id":"FHKST01010100"
+        }
+        params = {
+            "fid_cond_mrkt_div_code": market_code,
+            "fid_input_iscd": ticker
+        }
+        url = f"{self.BASE_URL}/{path}"
+        res = requests.get(url, headers=headers, params=params)
+        return res.json()
+
+
+if __name__ == "__main__":
+    import pprint
+
+    with open("../koreainvestment.key") as f:
+        lines = f.readlines()
+    
+    key = lines[0].strip()
+    secret = lines[1].strip()
+
+    ki = KoreaInvestment(key, secret)
+    resp = ki.fetch_price("J", "005930")
+    pprint.pprint(resp)
