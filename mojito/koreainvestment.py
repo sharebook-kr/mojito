44686dfb00ccbd54acb98eaa9aa83e95f41bf41c
Brayden Jo <brayden.jo@outlook.com>
2022-04-02 14:02:51 +0900

    fetch_balance

--- mojito/koreainvestment.py
+++ mojito/koreainvestment.py
@@ -52,7 +52,18 @@ class KoreaInvestment(Broker):
         res = requests.get(url, headers=headers, params=params)
         return res.json()
 
-    def fetch_daily_price(self, market_code: str, ticker: str, adj_price: bool=True) -> dict:
+    def fetch_daily_price(self, market_code: str, ticker: str, period: str='D', adj_price: bool=True) -> dict:
+        """주식 현재가 일자별
+
+        Args:
+            market_code (str): 시장 분류코드 
+            ticker (str): 종목코드
+            period (str): "D" (일), "W" (주), "M" (월)
+            adj_price (bool, optional): True: 수정주가 반영, False: 수정주가 미반영. Defaults to True.
+
+        Returns:
+            dict: _description_
+        """
         path = "uapi/domestic-stock/v1/quotations/inquire-daily-price"
         url = f"{self.BASE_URL}/{path}"
         headers = {
@@ -68,7 +79,41 @@ class KoreaInvestment(Broker):
             "fid_cond_mrkt_div_code": market_code,
             "fid_input_iscd": ticker,
             "fid_org_adj_prc": adj_param,
-            "fid_period_div_code":"D"
+            "fid_period_div_code": period
+        }
+        res = requests.get(url, headers=headers, params=params)
+        return res.json()
+
+    def fetch_balance(self, acc_no: str) -> dict:
+        """주식잔고조회
+
+        Args:
+            acc_no (str): 계좌번호 앞8자리
+
+        Returns:
+            dict: _description_
+        """
+        path = "/uapi/domestic-stock/v1/trading/inquire-balance" 
+        url = f"{self.BASE_URL}/{path}"
+        headers = {
+           "content-type": "application/json", 
+           "authorization": self.access_token,
+           "appKey": self.api_key,
+           "appSecret": self.api_secret,
+           "tr_id": "TTTC8434R"
+        }
+        params = {
+            'CANO': acc_no, 
+            'ACNT_PRDT_CD': '01', 
+            'AFHR_FLPR_YN': 'N', 
+            'OFL_YN': 'N', 
+            'INQR_DVSN': '01', 
+            'UNPR_DVSN': '01', 
+            'FUND_STTL_ICLD_YN': 'N', 
+            'FNCG_AMT_AUTO_RDPT_YN': 'N', 
+            'PRCS_DVSN': '01', 
+            'CTX_AREA_FK100': '', 
+            'CTX_AREA_NK100': ''
         }
         res = requests.get(url, headers=headers, params=params)
         return res.json()
@@ -89,4 +134,7 @@ if __name__ == "__main__":
     #pprint.pprint(resp)
     
     #resp = ki.fetch_daily_price("J", "005930")
+    #pprint.pprint(resp)
+
+    #resp = ki.fetch_balance("00000000")
     #pprint.pprint(resp)
