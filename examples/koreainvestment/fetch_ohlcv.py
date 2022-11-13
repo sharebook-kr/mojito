edb38e95805a4fdbcc4e6e2f488aa82db15e76da
Brayden Jo <brayden.jo@outlook.com>
2022-11-13 16:21:10 +0900

    fetch ohlcv domestic

--- examples/koreainvestment/fetch_ohlcv.py
+++ examples/koreainvestment/fetch_ohlcv.py
@@ -1,5 +1,6 @@
 import mojito
 import pprint
+import pandas as pd
 
 with open("../../koreainvestment.key") as f:
     lines = f.readlines()
@@ -19,5 +20,14 @@ resp = broker.fetch_ohlcv(
     timeframe='D',
     adj_price=True
 )
+
 pprint.pprint(resp)
 print(len(resp['output2']))
+
+df = pd.DataFrame(resp['output2'])
+dt = pd.to_datetime(df['stck_bsop_date'], format="%Y%m%d")
+df.set_index(dt, inplace=True)
+df = df[['stck_oprc', 'stck_hgpr', 'stck_lwpr', 'stck_clpr']]
+df.columns = ['open', 'high', 'low', 'close']
+df.index.name = "date"
+print(df)
